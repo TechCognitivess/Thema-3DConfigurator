498bf0bf7c18a024010b5a3883eac628ef4f2587
Techcognitives <Techcognitives>
2023-10-02 18:42:38 +0530

    2/10  latest commit

--- src/routes/thema.js
+++ src/routes/thema.js
@@ -3,38 +3,49 @@ const newsRouter = express.Router()
 const axios = require('axios')
 
 newsRouter.get('', async (req, res) => {
-    res.render('index', { articles: null })
+    res.render('index', {
+        articles: null
+    })
 })
-
 
 
 newsRouter.get('/home', async (req, res) => {
     try {
         const newsAPI = await axios.get(`http://173.255.119.18:4000/fronts/brands/igreen/brandType/eyeglasses`)
-        console.log("data.............."+newsAPI.data.Data[0].SAP_model_code)
-        if(newsAPI.data.Success==1){
-            res.render('home', { articles: newsAPI.data.Data})
+        console.log("data.............." + newsAPI.data.Data[0].SAP_model_code)
+
+        if (newsAPI.data.Success == 1) {
+            res.render('home', {
+                articles: newsAPI.data.Data
+            })
             console.log(newsAPI.Data)
-        }else{
+        } else {
             //res.render('home', { articles: null })
-           // console.log(err.requiest)
+            // console.log(err.requiest)
         }
-      
+
     } catch (err) {
         if (err.response) {
-            res.render('home', { articles: null })
+            res.render('home', {
+                articles: null
+            })
             console.log(err.response.data)
             console.log(err.response.status)
             console.log(err.response.headers)
         } else if (err.request) {
-            res.render('home', { articles: null })
+            res.render('home', {
+                articles: null
+            })
             console.log(err.requiest)
         } else {
-            res.render('home', { articles: null })
+            res.render('home', {
+                articles: null
+            })
             console.error('Error', err.message)
         }
     }
-   // res.render('home', { articles: null })
+    // res.render('home', { articles: null })
+
 })
 
 newsRouter.get('/o-six', async (req, res) => {
@@ -56,7 +67,9 @@ newsRouter.get('/o-six', async (req, res) => {
     //         console.error('Error', err.message)
     //     }
     // }
-    res.render('o-six', { articles: null })
+    res.render('o-six', {
+        articles: null
+    })
 })
 
 newsRouter.get('/giorgio', async (req, res) => {
@@ -78,7 +91,9 @@ newsRouter.get('/giorgio', async (req, res) => {
     //         console.error('Error', err.message)
     //     }
     // }
-    res.render('giorgio', { articles: null })
+    res.render('giorgio', {
+        articles: null
+    })
 })
 // newsRouter.get('/:id', async (req, res) => {
 //     let articleID = req.params.id
@@ -125,4 +140,4 @@ newsRouter.get('/giorgio', async (req, res) => {
 // })
 
 
-module.exports = newsRouter 
+module.exports = newsRouter
